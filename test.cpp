7fe0593d37b742a1aaa3b3e22bc7c79c0ba9ad7e
ckw1219 <121977511@qq.com>
2021-05-21 10:26:33 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "hello vs code" << endl;
+    system("pause");
+    return 0;
+}
+
+// "F:\\software_path\\mingw\\mingw64\\bin\\g++.exe"
